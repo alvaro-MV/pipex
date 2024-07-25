b7a6e829165f085f2b29070671603c9a3d1c0d8c
Álvaro Morales <moralesalvaro97@gmail.com>
2024-07-25 21:07:33 +0200

    Ahora tira hasta el wc -l, pero no printa una mierda

--- orueba.c
+++ orueba.c
@@ -1,4 +1,8 @@
 #include "pipex.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 void	execute_child(char **argv, char **path, char **arguments, int pipefd[2])
 {
@@ -8,6 +12,7 @@ void	execute_child(char **argv, char **path, char **arguments, int pipefd[2])
 	if (dup2(pipefd[1], 1) == -1)
 		(perror("dup"), exit(-1));
 	close(pipefd[1]);
+	close(pipefd[0]);
 	execve(arguments[0], arguments, NULL);
 	(perror("execve"), exit(-1));
 }
@@ -19,28 +24,49 @@ int	main(int argc, char **argv, char *env[])
 	int 	status;
 	char	**path;
 	char	**arguments;
+	int		fdd;
 
-	if (pipe(pipefd) == -1)
-	{
-		perror("pipex");
-		exit(-1);
-	}
+
 	path = get_PATH(env);
 	argv++;	
-	if (dup2(pipefd[0], 0) == -1)
-		(perror("dup"), exit(-1));
+//	if (dup2(pipefd[0], 0) == -1)
+		//(perror("dup"), exit(-1));
+	//close(pipefd[0]);
+	//close(pipefd[1]);
 	while (argv[1] != NULL)
 	{
+		if (pipe(pipefd) == -1)
+		{
+			perror("pipex");
+			exit(-1);
+		}
 		ret = fork();
-		if (ret == 0)
+		if (ret == -1)
+			(perror("fork"), exit(-1));
+		else if (ret == 0)
+		{
+			dup2(fdd, 0);
 			execute_child(argv, path, arguments, pipefd);
-		wait(&status);
-		++argv;
-		arguments = ft_split(*argv, ' ');
-		arguments[0] = find_exec_in_PATH(path, arguments[0]);
-		ft_printf("primer arg: %s\n", arguments[0]);
-		close(pipefd[0]);
+		}
+		else
+		{
+			wait(&status);
+			close(pipefd[1]);
+			fdd = pipefd[0];
+			++argv;
+		}
 	}
+	arguments = ft_split(*argv, ' ');
+	arguments[0] = find_exec_in_PATH(path, arguments[0]);
+	ft_printf("primer arg: %s\n", arguments[0]);
+	//testeo
+	int	idx = 0;
+	while (arguments[idx])
+	{
+		ft_printf("arg: %s", arguments[idx]);
+		idx++;
+	}
+	//testeo
 	execve(arguments[0], arguments, NULL);
 	(perror("execve"), exit(-1));
 	return (0);
