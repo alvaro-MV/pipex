0ba3b5d0ba083de8ca3238add3a2b0e821126a18
Álvaro Morales <moralesalvaro97@gmail.com>
2024-07-25 21:19:12 +0200

    Ahora tira, no me preguntes porque

--- orueba.c
+++ orueba.c
@@ -9,8 +9,11 @@ void	execute_child(char **argv, char **path, char **arguments, int pipefd[2])
 	arguments = ft_split(*argv, ' ');
 	arguments[0] = find_exec_in_PATH(path, arguments[0]);
 	ft_printf("primer arg: %s\n", arguments[0]);
-	if (dup2(pipefd[1], 1) == -1)
-		(perror("dup"), exit(-1));
+	if (argv[1] != NULL)
+	{
+		if (dup2(pipefd[1], 1) == -1)
+			(perror("dup"), exit(-1));
+	}
 	close(pipefd[1]);
 	close(pipefd[0]);
 	execve(arguments[0], arguments, NULL);
@@ -29,11 +32,7 @@ int	main(int argc, char **argv, char *env[])
 
 	path = get_PATH(env);
 	argv++;	
-//	if (dup2(pipefd[0], 0) == -1)
-		//(perror("dup"), exit(-1));
-	//close(pipefd[0]);
-	//close(pipefd[1]);
-	while (argv[1] != NULL)
+	while (argv[0] != NULL)
 	{
 		if (pipe(pipefd) == -1)
 		{
@@ -56,18 +55,19 @@ int	main(int argc, char **argv, char *env[])
 			++argv;
 		}
 	}
-	arguments = ft_split(*argv, ' ');
-	arguments[0] = find_exec_in_PATH(path, arguments[0]);
-	ft_printf("primer arg: %s\n", arguments[0]);
-	//testeo
-	int	idx = 0;
-	while (arguments[idx])
-	{
-		ft_printf("arg: %s", arguments[idx]);
-		idx++;
-	}
-	//testeo
-	execve(arguments[0], arguments, NULL);
-	(perror("execve"), exit(-1));
-	return (0);
+//	close(pipefd[0]);
+	//arguments = ft_split(*argv, ' ');
+	//arguments[0] = find_exec_in_PATH(path, arguments[0]);
+	//ft_printf("primer arg: %s\n", arguments[0]);
+	////testeo
+	//int	idx = 0;
+	//while (arguments[idx])
+	//{
+		//ft_printf("arg: %s", arguments[idx]);
+		//idx++;
+	//}
+	////testeo
+	//execve(arguments[0], arguments, NULL);
+	//(perror("execve"), exit(-1));
+	//return (0);
 }
